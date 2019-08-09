1fbd057fa17b89ff3abc92bd0fc750900f715a47
MintWay <mintway0341@gmail.com>
2019-08-10 00:02:32 +0900

    MintWay Theme Download

---
+++ mintway_theme_download.js
@@ -0,0 +1,13 @@
+function download()
+{
+    window.location.assign('MintWay.ktheme');
+}
+
+function change_color_1()
+{
+    document.getElementById("button_download").style.backgroundColor = '#0043A5';
+}
+function change_color_2()
+{
+    document.getElementById("button_download").style.backgroundColor = '#267EFF';
+}
